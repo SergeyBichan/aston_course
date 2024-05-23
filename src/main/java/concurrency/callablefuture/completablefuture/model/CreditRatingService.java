6f55663c53faadd696570fbcbb58864185df0ed4
SergeyBichan <ugidancer@gmail.com>
2024-05-23 15:08:19 +0300

    remove concurrency package

--- src/main/java/concurrency/callablefuture/completablefuture/model/CreditRatingService.java
+++
@@ -1,7 +0,0 @@
-package ru.aston.java.base.lesson7.concurrency.callablefuture.completablefuture.model;
-
-public class CreditRatingService {
-    public static Double getCreditRating(User user) {
-        return 1.1;
-    }
-}
--- src/main/java/concurrency/callablefuture/completablefuture/model/User.java
+++
@@ -1,8 +0,0 @@
-package ru.aston.java.base.lesson7.concurrency.callablefuture.completablefuture.model;
-
-public class User {
-    private String id;
-    public User(String userId) {
-        this.id = userId;
-    }
-}
