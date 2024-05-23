f7550c6381a4a66004a78243b7a955be3b7fb62e
SergeyBichan <ugidancer@gmail.com>
2024-05-23 17:50:16 +0300

    add nio reading to FileOutputStreamRunner

--- src/main/java/lesson_5/iostream/FileOutputStreamRunner.java
+++ src/main/java/lesson_5/iostream/FileOutputStreamRunner.java
@@ -2,6 +2,10 @@ package lesson_5.iostream;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileOutputStreamRunner {
     public static void main(String[] args) throws IOException {
@@ -9,7 +13,20 @@ public class FileOutputStreamRunner {
         try (FileOutputStream outputStream = new FileOutputStream("resources/text2.txt", true)) {
             String value = "Hello World2!";
             outputStream.write(value.getBytes());
-//            outputStream.write(System.lineSeparator().getBytes());
+            outputStream.write(System.lineSeparator().getBytes());
+        }
+
+        //using nio
+
+        try {
+            String value = "Hello World2!";
+            Path path = Paths.get("resources/text2.txt");
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+            Files.writeString(path, value);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
